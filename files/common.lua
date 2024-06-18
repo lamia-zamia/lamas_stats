6b7bd41f3ec3f3703ee70e4b0c6bbcde1c8d79b6
Lamia <xaxnas@gmail.com>
2024-06-18 18:55:01 +0100

    added dimension check for tooltip

--- files/common.lua
+++ files/common.lua
@@ -47,24 +47,50 @@ function GuiTextRed(gui, x, y, text, scale)
 	GuiText(gui, x, y, text, scale)
 end
 
+local gui_tooltip_size_cache = setmetatable({}, { __mode = "k" })
+
+local function ReturnZeroOrMinus(value)
+	if value < 0 then return value - 10
+	else return 0 end
+end
+
+local function GuiTooltipLamasValidateTooltipCache(x, y, z, action, passable_table, key)
+	if not gui_tooltip_size_cache[key] then
+		local phantom_gui = GuiCreate()
+		local offscreen_offset = 1000
+		GuiStartFrame(phantom_gui)
+		GuiLayoutBeginVertical(phantom_gui, x + offscreen_offset, y + offscreen_offset, true)
+		action(phantom_gui, passable_table)
+		GuiLayoutEnd(phantom_gui)
+		local screen_w,screen_h = GuiGetScreenDimensions(phantom_gui)
+		local _,_,_,x,y,w,h = GuiGetPreviousWidgetInfo(phantom_gui)
+		GuiDestroy(phantom_gui)
+		gui_tooltip_size_cache[key] = {x = ReturnZeroOrMinus(screen_w - x - w + offscreen_offset), y = ReturnZeroOrMinus(screen_h - y - h + offscreen_offset)}
+	end
+end
+
 function GuiTooltipLamas(gui, x, y, z, action, passable_table)
-	local _,_,gui_hovered,gui_x,gui_y = GuiGetPreviousWidgetInfo(gui)
+	local _,_,gui_hovered,gui_x,gui_y,gui_w,gui_h = GuiGetPreviousWidgetInfo(gui)
 	if gui_hovered then --immitating tooltip
+		local key = tostring(action) .. tostring(passable_table)
+		GuiTooltipLamasValidateTooltipCache(gui_x + gui_w + 10, gui_y + (gui_h/2) + y, z, action, passable_table, key)
 		GuiZSet(gui,-100)
 		GuiAnimateBegin(gui)
 		GuiAnimateScaleIn(gui, 555, 0.1, false)
+		
 		GuiLayoutBeginLayer(gui)
 		GuiBeginAutoBox(gui)
 		
-		GuiLayoutBeginVertical(gui, gui_x + x, gui_y + y, true)
-		
+		GuiLayoutBeginVertical(gui, gui_x + gui_w + x + 10, gui_y + (gui_h/2) + y + gui_tooltip_size_cache[key].y, true)
 		action(gui, passable_table)
-		
 		GuiLayoutEnd(gui)
+
 		
 		GuiZSetForNextWidget(gui, -99)
 		GuiEndAutoBoxNinePiece(gui)
+
 		GuiLayoutEndLayer(gui) 
+
 		GuiAnimateEnd(gui)
 		GuiZSet(gui, z)
 	end
