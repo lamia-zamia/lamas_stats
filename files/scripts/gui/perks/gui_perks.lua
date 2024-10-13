1b11142ae65c75dcc96ca3c928f1b443cf4399c7
Lamia <xaxnas@gmail.com>
2024-10-13 22:07:33 +0100

    more cheks

--- files/scripts/gui/perks/gui_perks.lua
+++ files/scripts/gui/perks/gui_perks.lua
@@ -5,6 +5,7 @@
 --- @field previous_window function|nil
 --- @field width number
 --- @field scrollbox_start number
+--- @field current_children_entities number
 
 --- @class (exact) LS_Gui
 --- @field perk LS_Gui_perks
@@ -15,7 +16,8 @@ local pg = {
 		current_window = nil,
 		previous_window = nil,
 		width = 0,
-		scrollbox_start = 0
+		scrollbox_start = 0,
+		current_children_entities = 0
 	}
 }
 
@@ -41,7 +43,6 @@ pg.perk.current_window = pg.PerksDrawCurrentPerkScrollBox
 --- @return boolean
 --- @nodiscard
 function pg:PerksIsHoverBoxHovered(x, y)
-	-- y = y + 
 	if y + 8 > 0 and y + 8 < self.scroll.height_max and self:IsHoverBoxHovered(self.menu.start_x + x - 3, self.perk.scrollbox_start + y, 16, 16)
 	then
 		return true
@@ -52,24 +53,19 @@ end
 --- Draws stats and perks window
 function pg:PerksDrawWindow()
 	local current_nearby_perks = #self.perks.nearby.entities
-	self.perks.nearby:Scan()
-	if current_nearby_perks ~= #self.perks.nearby.entities then
-		self:PerksUpdate()
-	end
+	self:CheckForUpdates(current_nearby_perks)
+
 	self.perk.x = self.menu.start_x
 	self.perk.y = self.menu.pos_y + 7
 
-	
 	self:PerksAddButton(T.lamas_stat_current, self.PerksDrawCurrentPerkScrollBox)
 	self:PerksAddButton(T.lamas_stats_perks_next, function() end)
 	self:PerksDrawStats()
 	self.perk.y = self.perk.y + 10
 
-	local header_height = 17
 	if current_nearby_perks > 0 then
 		self.perk.y = self.perk.y + 5
 		self:PerksDrawNearby()
-		-- header_height = header_height + 16
 		self.perk.y = self.perk.y + 12
 	end
 	self:Draw9Piece(self.menu.start_x - 6, self.menu.pos_y + 4, self.z + 49, self.scroll.width + 6, self.perk.y - self.menu.pos_y)
@@ -84,6 +80,21 @@ function pg:PerksDrawWindow()
 	self.perk.previous_window = self.perk.current_window
 end
 
+--- Checks if perks needs to update
+--- @private
+--- @param current_nearby_perks number
+function pg:CheckForUpdates(current_nearby_perks)
+	self.perks.nearby:Scan()
+	local current_children_entities = EntityGetAllChildren(self.player) or {}
+	local current_children_count = #current_children_entities
+	if current_nearby_perks ~= #self.perks.nearby.entities or current_children_count ~= self.perk.current_children_entities then
+		self:PerksUpdate()
+	end
+	self.perk.current_children_entities = current_children_count
+end
+
+--- Updates perks data
+--- @private
 function pg:PerksUpdate()
 	self.perks:GetCurrentList()
 	self.perks.nearby:ParseEntities()
@@ -93,10 +104,6 @@ end
 function pg:PerksInit()
 	self:PerksUpdate()
 	self.scroll.width = 203
-	-- self:FungalUpdateWindowDims()
-	-- self.fungal.past = self.mod:GetSettingBoolean("enable_fungal_past")
-	-- self.fungal.future = self.mod:GetSettingBoolean("enable_fungal_future")
-	-- self.scroll.width = self.fungal.width
 end
 
 return pg
